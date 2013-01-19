efb6a37c75b1c2f0b6c5542f8c419f41b148f396
bence <bence@voyager>
2013-01-19 01:39:16 +0100

    kuki

--- TronokHarca/src/Egyseg.java
+++ TronokHarca/src/Egyseg.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -64,7 +66,39 @@ public class Egyseg extends JLabel{
 		return s;
 	}
 		
+	MouseListener mL = new MouseListener() {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 		
+	};
 	
 
 }
